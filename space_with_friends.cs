edb2b8e6548b33ec59c3ece4793d90b2dab6534a
Andy Burke <andy.burke@mailbox.earth>
2021-05-28 11:34:30 -0700

    initial commit

---
+++ space_with_friends.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+namespace space_with_friends
+{
+
+    [KSPAddon(KSPAddon.Startup.Flight, once: false)]
+    public class space_with_friends : MonoBehaviour
+    {
+
+        public void Start()
+        {
+            utils.Log("starting...");
+
+            if ( FlightGlobals.ActiveVessel == null )
+            {
+                utils.Log("  no active vessel, skipping");
+                return;
+            }
+
+            /*
+            var controls = FindObjectsOfType<UI_Control>();
+            foreach (var module in FlightGlobals.ActiveVessel.vesselModules )
+            {
+                Log("    module: " + module.name);
+
+                foreach(var field in module.Fields)
+                {
+                    Log("      field: " + field.name);
+                    field.OnValueModified += onValueModified;
+                    field.uiControlFlight.onFieldChanged = onFieldChanged;
+                }
+            }
+            */
+
+        }
+
+        public void OnDestroy()
+        {
+        }
+
+        /*
+        public void onFieldChanged(BaseField field, object value)
+        {
+            Log("onFieldChanged: " + field.name + ": " + value);
+        }
+
+        public void onValueModified(object value)
+        {
+            Log("onValueModified: " + value.ToString());
+        }
+        */
+
+    }
+}
---
+++ utils.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace space_with_friends
+{
+    class utils
+    {
+        public static void Log(string msg)
+        {
+            Debug.Log("[space_with_friends] " + msg);
+        }
+    }
+}
