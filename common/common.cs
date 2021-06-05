1059cdd24fcc3b255b47da2f95f55c10514af337
Marc Hernandez <marc.hernandez@disruptivegames.com>
2021-06-04 20:59:59 -0700

    Bare skeletons

---
+++ common/common.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace space_with_friends {
+
+
+	public class common {
+
+		static public void test( string test ) {
+
+			Console.WriteLine( "hello" );
+
+		}
+
+
+
+	}
+
+
+
+}
---
+++ server/server.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace space_with_friends {
+
+    public class server{
+
+        static public void Main( string[] args ) {
+
+            Console.WriteLine( "space_with_friends.server.main" );
+
+            common.test( "hello" );
+        }
+    }
+
+}
