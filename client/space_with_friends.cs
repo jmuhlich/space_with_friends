bf4d142e3a6d5ef65914f10e88518ed03606ad46
Andy Burke <andy.burke@mailbox.earth>
2021-06-05 00:48:23 -0700

    feature: persisting settings

--- client/space_with_friends.cs
+++ client/space_with_friends.cs
@@ -2,16 +2,13 @@
 
 namespace space_with_friends {
 
-	[KSPAddon( KSPAddon.Startup.Flight, once: false )]
+	[KSPAddon( KSPAddon.Startup.AllGameScenes, once: true )]
 	public class space_with_friends : MonoBehaviour {
 
 		public void Start() {
 			utils.Log( "starting..." );
 
-			if (FlightGlobals.ActiveVessel == null) {
-				utils.Log( "  no active vessel, skipping" );
-				return;
-			}
+			utils.Log( "host: " + space_with_friends_settings.instance.host + " port: " + space_with_friends_settings.instance.port );
 		}
 
 		public void OnDestroy() {
---
+++ client/space_with_friends_settings.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using KSP;
+
+namespace space_with_friends {
+	[KSPScenario( ScenarioCreationOptions.AddToAllGames, GameScenes.SPACECENTER, GameScenes.EDITOR, GameScenes.FLIGHT )]
+	public class space_with_friends_settings : ScenarioModule {
+		public string host = "localhost";
+		public UInt16 port = 7887;
+
+		public static space_with_friends_settings instance;
+
+		public static void MainMenuBuildDefaultScenarioModule() {
+			if (instance == null) {
+				instance = new space_with_friends_settings();
+				utils.Log( "creating new settings module" );
+				instance.OnLoad( new ConfigNode() );
+				instance.Start();
+			}
+		}
+		space_with_friends_settings() {
+			instance = this;
+		}
+
+		void Start() {
+			instance = this;
+
+			utils.Log( "setup started" );
+			GameEvents.onGameStateSave.Add(OnSave);
+		}
+
+		void OnDestroy() {
+			//GamePersistence.SaveGame("persistent", HighLogic.SaveFolder, SaveMode.OVERWRITE);
+			GameEvents.onGameStateSave.Remove(OnSave);
+		}
+
+		public override void OnSave( ConfigNode node ) {
+			utils.Log( "saving" );
+			node.AddValue( "host", host );
+			node.AddValue( "port", port.ToString() );
+		}
+
+		public override void OnLoad( ConfigNode node ) {
+			instance = this;
+
+			if ( node.HasValue( "host" ) ) {
+				host = node.GetValue( "host" );
+			}
+
+			if ( node.HasValue( "port" ) ) {
+				port = UInt16.Parse( node.GetValue( "port" ) );
+			}
+
+			utils.Log( "loaded" );
+		}
+	}
+}
