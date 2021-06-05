e9f27a5254ee8350bd49809d8b1d7265d5eb329c
Marc Hernandez <marc.hernandez@disruptivegames.com>
2021-06-04 23:52:02 -0700

    Literally MVP in calling something a server.

---
+++ headless/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace client
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var c = new Client();
+			c.Start();
+		}
+	}
+}
---
+++ server/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace server
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			msg.ClientLogin loginDummy;
+
+			Server.Start();
+		}
+	}
+}
