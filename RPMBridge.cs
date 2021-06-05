2e40d48e873828d65e237bb271b739d18acf398a
Andy Burke <andy.burke@mailbox.earth>
2021-06-04 18:57:35 -0700

    refactor: move away from DMP refactor: try to switch to dotnet for building, etc.

---
+++ RPMBridge.cs
@@ -0,0 +1,51 @@
+﻿namespace space_with_friends
+{
+
+	[KSPAddon(KSPAddon.Startup.Flight, once: false)]
+	public class RPMBridge
+	{
+		public void Start()
+		{
+			JSI.Core.Events.onEvent += on_event;
+		}
+
+		public void OnDestroy()
+		{
+			JSI.Core.Events.onEvent -= on_event;
+		}
+
+		public static void on_event(JSI.Core.Event _event)
+		{
+			if (!(_event.name.StartsWith("click:") || _event.name.StartsWith("release:")))
+			{
+				return;
+			}
+
+			utils.Log("sending: " + _event.name);
+		}
+
+		void on_network_message(byte[] data)
+		{
+			string message;
+			try
+			{
+				message = System.Text.Encoding.ASCII.GetString(data);
+			}
+			catch (System.Exception ex)
+			{
+				UnityEngine.Debug.LogError(ex.StackTrace);
+				return;
+			}
+
+			if (!(message.StartsWith("click:") || message.StartsWith("release:")))
+			{
+				return;
+			}
+
+			utils.Log("replaying: " + message);
+
+			JSI.SmarterButton.Replay(message);
+		}
+	}
+}
+
