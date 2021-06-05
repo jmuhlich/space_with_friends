cf1a3deb4d80dbae8bdcd56b9d783bf135af6714
Andy Burke <andy.burke@mailbox.earth>
2021-06-04 20:54:29 -0700

    feature: build tasks

--- client/space_with_friends.cs
+++ client/space_with_friends.cs
@@ -2,48 +2,49 @@
 
 namespace space_with_friends {
 
-    [KSPAddon( KSPAddon.Startup.Flight, once: false )]
-    public class space_with_friends : MonoBehaviour {
+	[KSPAddon( KSPAddon.Startup.Flight, once: false )]
+	public class space_with_friends : MonoBehaviour {
 
-        public void Start() {
-            utils.Log( "starting..." );
+		public void Start() {
+			utils.Log( "starting..." );
 
-            if (FlightGlobals.ActiveVessel == null) {
-                utils.Log( "  no active vessel, skipping" );
-                return;
-            }
+			G
+			if (FlightGlobals.ActiveVessel == null) {
+				utils.Log( "  no active vessel, skipping" );
+				return;
+			}
 
-            /*
-            var controls = FindObjectsOfType<UI_Control>();
-            foreach (var module in FlightGlobals.ActiveVessel.vesselModules )
-            {
-                Log("    module: " + module.name);
+			/*
+			var controls = FindObjectsOfType<UI_Control>();
+			foreach (var module in FlightGlobals.ActiveVessel.vesselModules )
+			{
+				Log("    module: " + module.name);
 
-                foreach(var field in module.Fields)
-                {
-                    Log("      field: " + field.name);
-                    field.OnValueModified += onValueModified;
-                    field.uiControlFlight.onFieldChanged = onFieldChanged;
-                }
-            }
-            */
+				foreach(var field in module.Fields)
+				{
+					Log("     field: " + field.name);
+					field.OnValueModified += onValueModified;
+					field.uiControlFlight.onFieldChanged = onFieldChanged;
+				}
+			}
+			*/
 
-        }
+		}
 
-        public void OnDestroy() {
-        }
+		public void OnDestroy() {
+		}
 
-        /*
-        public void onFieldChanged(BaseField field, object value)
-        {
-            Log("onFieldChanged: " + field.name + ": " + value);
-        }
+		/*
+		public void onFieldChanged(BaseField field, object value)
+		{
+			Log("onFieldChanged: " + field.name + ": " + value);
+		}
 
-        public void onValueModified(object value)
-        {
-            Log("onValueModified: " + value.ToString());
-        }
-        */
+		public void onValueModified(object value)
+		{
+			Log("onValueModified: " + value.ToString());
+		}
+		*/
 
-    }
+	}
 }
